474fc6ebd1d9a0905c7a6b1866f7eb4e78c5da09
a-tomlinson <atomlinson91@googlemail.com>
2016-10-30 22:39:26 +0000

    done for the weekend

--- app/transitions.js
+++ app/transitions.js
@@ -2,12 +2,6 @@ export default function(){
   this.transition(
     this.fromRoute(['home','biography']),
     this.toRoute(['home','biography']),
-    this.use('to-down')
-  ),
-
-  this.transition(
-    this.fromRoute('index'),
-    this.toRoute('home'),
-    this.use('to-up')
+    this.use('fade')
   )
 };
