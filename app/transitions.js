57b29474417a099b6764dba8920c74a2bcd038b8
Roughprada <mprada.rafael@gmail.com>
2016-11-03 16:54:04 +0000

    updated transitions

--- app/transitions.js
+++ app/transitions.js
@@ -1,7 +1,7 @@
 export default function(){
   this.transition(
-    this.fromRoute(['home','biography']),
-    this.toRoute(['home','biography']),
+    this.fromRoute(['home','biography','galleries']),
+    this.toRoute(['home','biography','galleries']),
     this.use('fade')
   );
 }
